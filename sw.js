f47b56b5172c429e44417a229da7289cc3f25e97
eugene gallego <eugall@gmail.com>
2026-04-19 13:17:03 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'raysland-radio';
+
+// Files to cache (adjust if needed)
+const ASSETS = [
+'/raysland-radio/',
+'/raysland-radio/index.html',
+'/raysland-radio/image.html'
+];
+
+// Install: cache app shell
+self.addEventListener('install', event => {
+event.waitUntil(
+caches.open(CACHE_NAME).then(cache => {
+return cache.addAll(ASSETS);
+})
+);
+self.skipWaiting();
+});
+
+// Activate: clean old caches
+self.addEventListener('activate', event => {
+event.waitUntil(
+caches.keys().then(keys => {
+return Promise.all(
+keys.filter(key => key !== CACHE_NAME)
+.map(key => caches.delete(key))
+);
+})
+);
+self.clients.claim();
+});
+
+// Fetch: cache-first strategy
+self.addEventListener('fetch', event => {
+event.respondWith(
+caches.match(event.request).then(response => {
+return response || fetch(event.request).then(networkResponse => {
+return caches.open(CACHE_NAME).then(cache => {
+cache.put(event.request, networkResponse.clone());
+return networkResponse;
+});
+});
+}).catch(() => {
+return caches.match('/efg-movie-hub/index.html');
+})
+);
+});
+
+
